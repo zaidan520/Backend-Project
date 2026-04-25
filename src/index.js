766b209f0fecc40fed50ab6db2afdc0b7af76336
Ali Zaidan <zaidanal0100@gmail.com>
2026-04-25 16:58:07 +0500

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//require('dotenv').config({path: './env'});
+import mongoose from "mongoose";
+import connectDB from './db/index.js';
+import dotenv from "dotenv"
+
+// import express from "express";
+// const app = express()
+// (async()=>{
+//     try {
+//       await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//       app.on("error", (error)=>{
+//         console.log("ERRR: ", error);
+//         throw error
+//       })
+//       app.listen(process.env.PORT, ()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//       })
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
+dotenv.config({
+    path: './.env'
+})
+connectDB();
