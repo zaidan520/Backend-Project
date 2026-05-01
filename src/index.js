b130d77294f98204071093156d4e50f4dc4118e4
Ali Zaidan <zaidanal0100@gmail.com>
2026-05-01 16:05:45 +0500

    cors and cookie-parser and express server

--- src/index.js
+++ src/index.js
@@ -23,4 +23,12 @@ import dotenv from "dotenv"
 dotenv.config({
     path: './.env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED !!!",err)
+})
