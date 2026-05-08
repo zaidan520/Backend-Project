ff970ff54ad4734b7e85c948ab26f426fda5a694
Ali Zaidan <zaidanal0100@gmail.com>
2026-05-08 16:03:36 +0500

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const  anyncHandler = (requestHandler)=>{
+    (req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+    }
+}
+ export {anyncHandler}
+
+// const asyncHandler = (fn) => (req, res, next)=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+        
+//     }
+
+// }
+
